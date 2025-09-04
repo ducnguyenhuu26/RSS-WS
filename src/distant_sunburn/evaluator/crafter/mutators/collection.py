2256c9b8509951dcc6fe7349e45551f431bfe4b6
Zaid Khan <brain@zaidkhan.me>
2025-09-03 20:49:20 -0400

    added tests for illegal material mutators

---
+++ src/distant_sunburn/evaluator/crafter/mutators/collection.py
@@ -0,0 +1,104 @@
+from .v1 import Mutator
+from ....typing_utils import implements
+from crafter.state_export import WorldState
+from crafter.constants import ActionT, CollectableT, collect
+import random
+from typing import cast
+from crafter.functional_env import transition
+from distant_sunburn.evaluator.crafter.utils import MAP_ACTION_TO_INDEX
+from loguru import logger
+from typing_extensions import assert_never
+from crafter.constants import MaterialT, materials
+
+TILE_TO_MATERIALS: dict[MaterialT, CollectableT] = {
+    "tree": "wood",
+    "stone": "stone",
+    "coal": "coal",
+    "iron": "iron",
+    "diamond": "diamond",
+    "water": "drink",
+    "grass": "sapling",
+}
+
+HARVESTABLE_TILES: set[MaterialT] = set(TILE_TO_MATERIALS.keys())
+
+
+class CollectIllegalMaterialMutator:
+    def __init__(self):
+        self.category = "Collection"
+        self.logger = logger.bind(mutator=self.__class__.__name__)
+
+    def precondition(self, state: WorldState, action: ActionT) -> bool:
+        # We make a choice here to treat any "do" action as a collection action.
+        # This is not always the case since "do" is contextual to the facing
+        # tile. For example, is the player is facing a entity, then the
+        # do action might result in combat with the entity.
+        return action == "do"
+
+    def __call__(self, state: WorldState, action: ActionT) -> WorldState:
+        mutated_state = state.model_copy(deep=True)
+
+        # The thing we have to do here is to make sure we are not
+        # collecting a material that the player _should_ be able to
+        # collect in the true next state.
+
+        # First, we check what the player is facing.
+        material, _ = state.get_target_tile()
+
+        if material is None:
+            # We're facing the edge of the world, thus anything we
+            # can collect is illegal.
+            unobtainable_tile = random.choice(list(HARVESTABLE_TILES))
+            illegal_collectable = TILE_TO_MATERIALS[unobtainable_tile]
+        else:
+            # We have a material! We will now pretend like the tile we are
+            # facing is actually one of the _other_ materials, specifically,
+            # a tile material that is harvestable.
+
+            disjunction = HARVESTABLE_TILES - cast(set[MaterialT], {material})
+
+            unobtainable_tile = random.choice(list(disjunction))
+
+            illegal_collectable = TILE_TO_MATERIALS[unobtainable_tile]
+
+        self.logger.debug(
+            f"Pretending {material} is actually {unobtainable_tile} and collecting {illegal_collectable}"
+        )
+
+        # Now we increment the player's inventory by 1 of the illegal collectable.
+        match illegal_collectable:
+            case "wood":
+                mutated_state.player.inventory.wood += 1
+            case "stone":
+                mutated_state.player.inventory.stone += 1
+            case "coal":
+                mutated_state.player.inventory.coal += 1
+            case "iron":
+                mutated_state.player.inventory.iron += 1
+            case "diamond":
+                mutated_state.player.inventory.diamond += 1
+            case "drink":
+                mutated_state.player.inventory.drink += 1
+            case "sapling":
+                mutated_state.player.inventory.sapling += 1
+            case "grass":
+                mutated_state.player.inventory.sapling += 1
+            case _:
+                logger.warning(
+                    f"Received a collectable {illegal_collectable} that cannot be added to the inventory; "
+                    f"this mutator should only be applied to collectable materials. "
+                    f".precondition() returns {self.precondition(state, action)} for this (s,a) pair."
+                    f" Setting all items to -1 to create a guaranteed illegal state."
+                )
+                mutated_state.player.inventory.wood = -1
+                mutated_state.player.inventory.stone = -1
+                mutated_state.player.inventory.coal = -1
+                mutated_state.player.inventory.iron = -1
+                mutated_state.player.inventory.diamond = -1
+                mutated_state.player.inventory.drink = -1
+                mutated_state.player.inventory.sapling = -1
+
+        return mutated_state
+
+
+implements(Mutator)(CollectIllegalMaterialMutator)
---
+++ tests/evaluator/mutators/test_collection.py
@@ -0,0 +1,133 @@
+from crafter.state_export import WorldState
+from crafter.functional_env import (
+    initial_state,
+    reconstruct_world_from_state,
+    export_world_state,
+    transition,
+)
+import copy
+from crafter.testing_helpers import player_utils, world_utils
+from distant_sunburn.evaluator.crafter.utils import MAP_ACTION_TO_INDEX
+import pytest
+from crafter.constants import ActionT
+from crafter import engine as crafter_engine
+from distant_sunburn.evaluator.crafter.mutators.collection import (
+    CollectIllegalMaterialMutator,
+    TILE_TO_MATERIALS,
+)
+from distant_sunburn.evaluator.crafter.utils import find_player
+import numpy as np
+from crafter.constants import actions
+from crafter.constants import MaterialT
+from crafter import objects as crafter_objects
+from crafter.constants import actions
+from typing import cast
+
+
+def make_collection_state(
+    target_material: MaterialT,
+    add_entity_on_target_tile: bool = False,
+) -> tuple[WorldState, crafter_engine.World]:
+    view = (9, 9)
+    state = initial_state(area=(9, 9), view=view, seed=1)
+    world = reconstruct_world_from_state(state)
+
+    player = find_player(world)
+    player_utils.set_player_position(player, (5, 5))
+
+    # Clear all the other tiles around the world to be grass
+    for x in range(view[0]):
+        for y in range(view[1]):
+            world_utils.set_tile_material(world, (x, y), "grass")
+
+    # Set the tile to the right of the player to the target material for collection
+    world_utils.set_tile_material(world, (6, 5), target_material)
+
+    if add_entity_on_target_tile:
+        world_utils.add_object_to_world(world, crafter_objects.Cow, (6, 5))
+
+    # Make the player face the target material
+    player_utils.set_player_facing(player, (1, 0))
+
+    # Give the player required resources to collect anything
+    player_utils.set_player_inventory_item(player, "iron_pickaxe", 1)
+
+    return export_world_state(world, view=view, step_count=0), world
+
+
+@pytest.mark.parametrize(
+    "material",
+    list(TILE_TO_MATERIALS.keys()),
+)
+def test_collection_illegal_material_mutator(material: MaterialT):
+    collection_state, _ = make_collection_state(material)
+
+    mutator = CollectIllegalMaterialMutator()
+
+    assert mutator.precondition(collection_state, "do")
+
+    true_next_state, _ = transition(
+        copy.deepcopy(collection_state), MAP_ACTION_TO_INDEX["do"]
+    )
+
+    mutated_next_state = mutator(collection_state, "do")
+
+    # Now we compare the two inventories. The mutated inventory
+    # should have an item that the true next state does not, and
+    # additionally, the mutation will not have consumed any resources.
+
+    assert mutated_next_state.player.inventory != true_next_state.player.inventory
+
+    # We do this check just to confirm that the __eq__ method is working
+    # correctly.
+    assert true_next_state.player.inventory == copy.deepcopy(
+        true_next_state.player.inventory
+    ), (
+        "The __eq__ method on inventories should assert equality when the inventories are the same"
+        " but this is not the case; this test is invalid."
+    )
+
+
+# We could do this in one pytest.mark.parametrize, but it's clearer to
+# separate these so the parameterization is easier to reads.
+@pytest.mark.parametrize("material", list(TILE_TO_MATERIALS.keys()))
+def test_collection_illegal_material_mutator_with_entity(material: MaterialT):
+    collection_state, _ = make_collection_state(
+        material, add_entity_on_target_tile=True
+    )
+
+    mutator = CollectIllegalMaterialMutator()
+
+    assert mutator.precondition(collection_state, "do")
+
+    true_next_state, _ = transition(
+        copy.deepcopy(collection_state), MAP_ACTION_TO_INDEX["do"]
+    )
+
+    mutated_next_state = mutator(collection_state, "do")
+
+    # Now we compare the two inventories. The mutated inventory
+    # should have an item that the true next state does not, and
+    # additionally, the mutation will not have consumed any resources.
+
+    assert mutated_next_state.player.inventory != true_next_state.player.inventory
+
+    # We do this check just to confirm that the __eq__ method is working
+    # correctly.
+    assert true_next_state.player.inventory == copy.deepcopy(
+        true_next_state.player.inventory
+    ), (
+        "The __eq__ method on inventories should assert equality when the inventories are the same"
+        " but this is not the case; this test is invalid."
+    )
+
+
+@pytest.mark.parametrize("action", list(set(actions) - cast(set[ActionT], {"do"})))
+def test_collection_illegal_material_mutator_inactive_on_non_do_actions(
+    action: ActionT,
+):
+    collection_state, _ = make_collection_state("grass", add_entity_on_target_tile=True)
+
+    mutator = CollectIllegalMaterialMutator()
+
+    assert not mutator.precondition(collection_state, action)
