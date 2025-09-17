5d903b7ae587fceb986f2c469001a66e2faef2e5
Zaid Khan <brain@zaidkhan.me>
2025-09-17 18:54:04 -0400

    added a entity health mutator

--- src/distant_sunburn/evaluator/crafter/mutators/__init__.py
+++ src/distant_sunburn/evaluator/crafter/mutators/__init__.py
@@ -3,14 +3,20 @@ from .crafting import CraftIllegalItemMutator
 from .movement import IllegalMovementMutator
 from .entity_position import EntityPositionMutator
 from .interface import Mutator
-from .entity_health import PlayerHealthMutator
+from .entity_health import PlayerHealthMutator, EntityHealthMutator
 from .player import InventoryMutator
 
-DEFAULT_MUTATORS = [
-    CollectIllegalMaterialMutator(),
-    CraftIllegalItemMutator(),
-    IllegalMovementMutator(),
+ALWAYS_ON_MUTATORS = [
+    EntityHealthMutator(),
     EntityPositionMutator(),
     PlayerHealthMutator(),
     InventoryMutator(),
 ]
+
+CONDITIONAL_MUTATORS = [
+    CollectIllegalMaterialMutator(),
+    CraftIllegalItemMutator(),
+    IllegalMovementMutator(),
+]
+
+DEFAULT_MUTATORS = ALWAYS_ON_MUTATORS + CONDITIONAL_MUTATORS
--- src/distant_sunburn/evaluator/crafter/mutators/entity_health.py
+++ src/distant_sunburn/evaluator/crafter/mutators/entity_health.py
@@ -2,6 +2,7 @@ from ....typing_utils import implements
 from .interface import Mutator
 from crafter.state_export import WorldState
 from crafter.constants import ActionT
+import random
 
 
 class PlayerHealthMutator:
@@ -24,3 +25,30 @@ class PlayerHealthMutator:
 
 
 implements(Mutator)(PlayerHealthMutator)
+
+
+class EntityHealthMutator:
+    def __init__(self):
+        self.category = "Combat"
+
+    def precondition(self, state: WorldState, action: ActionT) -> bool:
+        return True
+
+    def __call__(self, state: WorldState, action: ActionT) -> WorldState:
+        mutated_state = state.model_copy(deep=True)
+
+        for entity in mutated_state.objects:
+            if entity.entity_id == mutated_state.player.entity_id:
+                continue
+
+            # Choose a random value for the health that is not the
+            # current health or a value that is within 1 of the current health
+            values = {_ for _ in range(0, 11)}
+            values = values - {entity.health, entity.health - 1, entity.health + 1}
+
+            entity.health = random.choice(list(values))
+
+        return mutated_state
+
+
+implements(Mutator)(EntityHealthMutator)
